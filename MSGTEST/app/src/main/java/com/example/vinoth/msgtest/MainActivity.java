cd91081733380aec3368875632a040aa7a61f3ea
VINOTH <vinoth.kmr0492@yahoo.com>
2019-03-20 10:01:01 +0530

    added messaging app

--- MSGTEST/app/src/main/java/com/example/vinoth/msgtest/MainActivity.java
+++ MSGTEST/app/src/main/java/com/example/vinoth/msgtest/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.vinoth.msgtest;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar myToolbar;
+
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    private FirebaseUser currentuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("MY CHAT POC");
+
+        myViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout)findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(currentuser == null){
+            SendUserToLoginActivity();
+        }
+    }
+
+    public void SendUserToLoginActivity(){
+        Intent intend = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intend);
     }
 }
