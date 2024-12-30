5f25e9cd9ef47653247aae760674297056853ebf
vinot <vinoth.kmr0492@yahoo.com>
2024-12-30 17:49:28 +0530

    vgposreports

---
+++ VGPOS/app/src/main/java/com/example/vgposrpt/BillDetails.java
@@ -0,0 +1,14 @@
+package com.example.vgposrpt;
+
+import java.util.ArrayList;
+
+public class BillDetails {
+    public ArrayList<Product> billProducts;
+    public Integer branchCode;
+    public String counter = "CD1";
+    public String billUser;
+    public Double BillAmount;
+    public Double CashAmt;
+    public Double CardAmt;
+    public Double UpiAmt;
+}
