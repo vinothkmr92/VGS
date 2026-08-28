9f3eddf79fad55a5fa3c9a1ebe099c8ac558352e
vinot <vinoth.kmr0492@yahoo.com>
2026-08-28 11:32:58 +0530

    ss

--- PMS/app/src/main/java/com/example/vinoth/pms/DatabaseHelper.java
+++ PMS/app/src/main/java/com/example/vinoth/pms/DatabaseHelper.java
@@ -19,7 +19,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public  static  final String DATABASE_NAME = "VGSPOS.db";
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 30);
+        super(context, DATABASE_NAME, null, 20);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
